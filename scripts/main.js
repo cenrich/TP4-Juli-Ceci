2e52e49e04da74f34b0f3b46954eb1962e38a405
cenrich <ceci_enrich@hotmail.com>
2019-08-24 12:42:40 -0300

    le pegué las funciones de ayer

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,5 @@
 // Limpiar el input cuando salgo del focus
 const apiKey="f800b0f5b9ae24d9ff462e770da4d3b3"
-const movieId=429203
 
 
 fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`)
@@ -9,13 +8,13 @@ fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`)
         console.log(res.results[0]) 
         const popularNode = document.getElementById("popularList")
         const topFive = res.results.filter((e,i)=>i<5)
-        topFive.forEach(({title,poster_path})=>{
+        topFive.forEach(({title,poster_path, id})=>{
             const a = document.createElement("a")
             a.classList.add("movieLink")
             a.href="#"
             const li = document.createElement("li")
             li.classList.add("movieBox")
-            li.onclick = () =>toggleFunction()
+            li.onclick = () =>toggleFunction(id)
             const image = document.createElement("img")
             image.src=`https://image.tmdb.org/t/p/w500/${poster_path}`
             image.classList.add("moviePoster")
@@ -29,29 +28,84 @@ fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`)
         });
     })
 
-fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}`)
+const loadModal = (movieId) =>{
+    fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}`)
     .then(response => response.json())
     .then(res => {
         // console.log(`https://image.tmdb.org/t/p/w500${res.poster_path}`)
         const backgroundNode=document.getElementById("upperModal")
-        backgroundNode.style.backgroundImage = "url(`https://image.tmdb.org/t/p/w500${res.poster_path}`)";
+        // backgroundNode.style.backgroundImage = "url(`https://image.tmdb.org/t/p/w500${res.poster_path}`)";
         const mainTitleNode = document.getElementById("mainTitle")
         mainTitleNode.innerText = res.title
         const descriptionNode =document.getElementById("movieDescription")
         descriptionNode.innerText=res.overview
         const genreNode = document.getElementById("genre")
-        res.genres.forEach(({name})=>genreNode.innerText+=`${name}
-                                                                    `)
+        genreNode.innerText=""
+        const genreList = []
+        res.genres.forEach(({name})=>genreList.push(name))
+        genreNode.innerText= genreList.join(", ")
         const releaseDateNode = document.getElementById("releaseDate")
         releaseDateNode.innerText = res.release_date 
     })
+}
 
-const toggleFunction = () => {
+const toggleFunction = (movieId) => {
     var modal = document.getElementById("modalContainer");
     if (modal.style.display === "none") {
+        loadModal(movieId)
         modal.style.display = "block";
     } else {
         modal.style.display = "none";
     }
 }
 
+const searchFunction = () => {
+    let query = event.target.value
+    if (query.length>=3 || (event.keyCode===13 && query!==lastRequest)) {
+        lastRequest=query
+        fetch (`https://api.themoviedb.org/3/search/movie?${apiKey}&query=${query}`)
+            .then(res=>res.json())
+            .then(res=>printResults(res.results))
+    }
+}
+
+const printResults = (movies) => {
+    const container = document.getElementById("results")
+    container.innerHTML=""
+    movies.forEach (movie=> {
+        let mov = document.createElement("li")
+        mov.innerText = movie.title === movie.original_title? movie.title : `${movie.title} (${movie.original_title})`
+        mov.onclick = () => toggleFunction(movie.id)
+        container.appendChild(mov)
+    })
+}
+
+const selectCategory = (category) => {
+    fetch (`https://api.themoviedb.org/3/movie/${category}?api_key=${apiKey}`)
+        .then(res=>res.json())
+        .then(res=>printCategory(res.results))
+}
+
+const printCategory = (movies) => {
+    let container = document.getElementById("categoryResults")
+    container.innerHTML=""
+    //ojo que de acá para adelante está repetido con la primera
+    movies.forEach(({title,poster_path, id})=>{
+        const a = document.createElement("a")
+        a.classList.add("movieLink")
+        a.href="#"
+        const li = document.createElement("li")
+        li.classList.add("movieBox")
+        li.onclick = () =>toggleFunction(id)
+        const image = document.createElement("img")
+        image.src=`https://image.tmdb.org/t/p/w500/${poster_path}`
+        image.classList.add("moviePoster")
+        a.appendChild(image)
+        const movieTitle = document.createElement("span")
+        movieTitle.innerText=title
+        movieTitle.classList.add("movieTitle")
+        a.appendChild(movieTitle)
+        li.appendChild(a)
+        container.appendChild(li)
+    });
+}
