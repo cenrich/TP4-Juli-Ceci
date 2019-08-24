b80cebcc5f7dbdef3127e6372647415244e86fdf
cenrich <ceci_enrich@hotmail.com>
2019-08-23 21:49:08 -0300

    con un modal estático

--- scripts/main.js
+++ scripts/main.js
@@ -10,16 +10,21 @@ fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`)
         const popularNode = document.getElementById("popularList")
         const topFive = res.results.filter((e,i)=>i<5)
         topFive.forEach(({title,poster_path})=>{
+            const a = document.createElement("a")
+            a.classList.add("movieLink")
+            a.href="#"
             const li = document.createElement("li")
             li.classList.add("movieBox")
+            li.onclick = () =>toggleFunction()
             const image = document.createElement("img")
             image.src=`https://image.tmdb.org/t/p/w500/${poster_path}`
             image.classList.add("moviePoster")
-            li.appendChild(image)
+            a.appendChild(image)
             const movieTitle = document.createElement("span")
             movieTitle.innerText=title
             movieTitle.classList.add("movieTitle")
-            li.appendChild(movieTitle)
+            a.appendChild(movieTitle)
+            li.appendChild(a)
             popularNode.appendChild(li)
         });
     })
@@ -29,10 +34,9 @@ fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}`)
     .then(res => {
         // console.log(`https://image.tmdb.org/t/p/w500${res.poster_path}`)
         const backgroundNode=document.getElementById("upperModal")
-        // backgroundNode.style.backgroundImage = "url(`https://image.tmdb.org/t/p/w500${res.poster_path}`)";
+        backgroundNode.style.backgroundImage = "url(`https://image.tmdb.org/t/p/w500${res.poster_path}`)";
         const mainTitleNode = document.getElementById("mainTitle")
         mainTitleNode.innerText = res.title
-        // const sloganNode = res.title
         const descriptionNode =document.getElementById("movieDescription")
         descriptionNode.innerText=res.overview
         const genreNode = document.getElementById("genre")
@@ -40,5 +44,14 @@ fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}`)
                                                                     `)
         const releaseDateNode = document.getElementById("releaseDate")
         releaseDateNode.innerText = res.release_date 
-                
     })
+
+const toggleFunction = () => {
+    var modal = document.getElementById("modalContainer");
+    if (modal.style.display === "none") {
+        modal.style.display = "block";
+    } else {
+        modal.style.display = "none";
+    }
+}
+
